252df6446451a612935307b44a5eac3b9e1288db
Mr-Hockatt <53312754+Mr-Hockatt@users.noreply.github.com>
2023-01-10 00:45:52 -0500

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,14 +1,16 @@
-from NeuronLayer import NeuronLayer
 from ActivationFunctions import Sigmoid
+from NeuronLayer import NeuronLayer
+import numpy as np
 
 class NeuralNetwork:
 
-    def __init__(self, number_of_inputs, dense_layer_shape, number_of_outputs):
+    def __init__(self, number_of_inputs, dense_layer_shape, number_of_outputs, cost_function):
 
         self.number_of_inputs = number_of_inputs
         self.number_of_outputs = number_of_outputs
         self.neurons_per_layer, self.number_of_dense_layers = dense_layer_shape
         self.layers = [None] * (self.number_of_dense_layers + 1)
+        self.cost_function = cost_function()
 
         self.initialize_architecture()
 
@@ -21,8 +23,8 @@ class NeuralNetwork:
 
             NOTES:
                 - The 1st layer has the same number of input connections as the number of inputs of the system.
-                - The output layer is also a layer, therefore the total number of layers is /number_of_dense_layers/ + 1.
                 - Each dense layer has N neurons and N number of input connections.
+                - The output layer is also a layer, therefore the total number of layers is /number_of_dense_layers/ + 1.
         """
 
         self.layers[0] = NeuronLayer(self.number_of_inputs, self.neurons_per_layer, Sigmoid)
@@ -47,6 +49,85 @@ class NeuralNetwork:
 
         for i in range(1, self.number_of_dense_layers + 1):
 
-            self.layers[i].feed_forward(self.layers[i - 1].output)
+            self.layers[i].feed_forward(self.layers[i - 1].activation)
 
-        return self.layers[-1].output
+        return self.layers[-1].activation
+
+    def update_weights(self, l, dC_dWl, alpha):
+
+        """
+            Implements the update formula for Backpropagation algorithm.
+
+            W_l = W_l - alpha * dC_dWl
+
+            Where:
+
+                l: Layer index
+                alpha: Learning rate
+                W_l: Weights in layer l
+                dC_dWl: Partial derivative of the cost function with respect to W_l
+        """
+
+        self.layers[l].weights = self.layers[l].weights - alpha * dC_dWl
+
+    def update_biases(self, l, dC_dBl, alpha):
+
+        """
+            Implements the update formula for Backpropagation algorithm.
+
+            B_l = B_l - alpha * dC_dBl
+
+            Where:
+
+                l: Layer index
+                alpha: Learning rate
+                B_l: Biases in layer l
+                dC_dBl: Partial derivative of the cost function with respect to B_l
+        """
+
+        self.layers[l].biases = self.layers[l].biases - alpha * np.mean(dC_dBl, axis=0).reshape(self.layers[l].biases.shape)
+
+    def back_propagate(self, input_data, output_data, learning_rate):
+
+        """
+            Backpropagates the error, layer by layer in a Gradient Descent fashion.
+        """
+
+        dC_dAl = self.cost_function.cost_derivative(output_data, self.layers[-1].activation)
+        dAl_dZl = self.layers[-1].activation_function.activation_derivative(self.layers[-1].output)
+        dZl_dWl = self.layers[-2].activation
+
+        delta_l = dC_dAl * dAl_dZl
+
+        self.update_biases(-1, delta_l, learning_rate)
+
+        dC_dWl = dZl_dWl.T @ delta_l
+
+        for l in reversed(range(1, self.number_of_dense_layers)):
+
+            dZlplus1_dAl = self.layers[l + 1].weights
+            dAl_dZl = self.layers[l].activation_function.activation_derivative(self.layers[l].output)
+            dZl_dWl = self.layers[l - 1].activation
+
+            delta_l = delta_l @ dZlplus1_dAl.T * dAl_dZl
+
+            self.update_biases(l, delta_l, learning_rate)
+
+            self.update_weights(l + 1, dC_dWl, learning_rate)
+
+            dC_dWl = dZl_dWl.T @ delta_l
+
+        l = 0
+
+        dZlplus1_dAl = self.layers[l + 1].weights
+        dAl_dZl = self.layers[l].activation_function.activation_derivative(self.layers[l].output)
+        dZl_dWl = input_data
+
+        delta_l = delta_l @ dZlplus1_dAl.T * dAl_dZl
+        self.update_biases(l, delta_l, learning_rate)
+
+        self.update_weights(l + 1, dC_dWl, learning_rate)
+
+        dC_dWl = dZl_dWl.T @ delta_l
+
+        self.update_weights(l, dC_dWl, learning_rate)
