0b3315ffd03ab20d8030a52421c746042d8d7202
Mr-Hockatt <53312754+Mr-Hockatt@users.noreply.github.com>
2023-01-10 00:46:48 -0500

    Update CostFunctions.py

--- CostFunctions.py
+++ CostFunctions.py
@@ -8,13 +8,17 @@ class MeanSquaredError:
 
     def cost(self, y, y_estimate):
 
+        number_of_samples, number_of_outputs = y.shape
+
         error = (y_estimate - y)
         mean_squared_error = (1/2) * np.mean(error**2, axis=0)
 
-        return mean_squared_error
+        return mean_squared_error.reshape(1, number_of_outputs)
 
     def cost_derivative(self, y, y_estimate):
 
+        number_of_samples, number_of_outputs = y.shape
+
         error = (y_estimate - y)
 
-        return np.mean(error, axis=0)
+        return np.mean(error, axis=0).reshape(1, number_of_outputs)
